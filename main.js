a5a6058f002a5b1e4125099114720c02d38e657b
david-senra <72723579+david-senra@users.noreply.github.com>
2023-04-24 22:22:48 -0300

    Edit main.js
    Correção na lógica do programa para detectar números de telefone iguais.

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const formulario = document.getElementById("formulario-inputs");
 let linhas = "";
 let nomesCadastrados = [];
-let dddsCadastrados = [];
-let telefonesCadastrados = [];
+let numerosCadastrados = [];
 
 formulario.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -12,23 +11,25 @@ formulario.addEventListener("submit", function(e) {
 })
 
 function adicionaLinha() {
-    const inputNomeContato = document.getElementById("nome-contato");
-    const inputDDDContato = document.getElementById("ddd-contato");
-    const inputTelContato = document.getElementById("tel-contato");
+    let inputNomeContato = document.getElementById("nome-contato");
+    let inputDDDContato = document.getElementById("ddd-contato");
+    let inputTelContato = document.getElementById("tel-contato");
+    let inputNumeroContato = parseInt(`${inputDDDContato.value}${inputTelContato.value}`);
+    console.log(inputNumeroContato);
 
     if (nomesCadastrados.includes(inputNomeContato.value)) {
         (alert(`O contato ${inputNomeContato.value} já foi cadastrado!`))
     }
-    else if (telefonesCadastrados.includes(inputTelContato.value)){
-        (alert(`O número de telefone ${inputTelContato.value} já foi cadastrado!`))
+    else if (numerosCadastrados.includes(inputNumeroContato)){
+        (alert(`O número de telefone (${inputDDDContato.value}) ${inputTelContato.value.substring(0, 5)}-${inputTelContato.value.substring(5, 9)} já foi cadastrado!`))
     }
     else {
         nomesCadastrados.push(inputNomeContato.value);
-        telefonesCadastrados.push(parseFloat(inputTelContato.value));
+        numerosCadastrados.push(inputNumeroContato);
         
         let linha = "<tr>";
         linha += `<td>${inputNomeContato.value}</td>`;
-        linha += `<td>(${inputDDDContato.value}) ${inputTelContato.value.slice(0,5)}-${inputTelContato.value.slice(5,9)}</td>`;
+        linha += `<td>(${inputDDDContato.value}) ${inputTelContato.value.substring(0, 5)}-${inputTelContato.value.substring(5, 9)}</td>`;
         linha += `</tr>`;
 
         linhas += linha;
@@ -36,6 +37,7 @@ function adicionaLinha() {
     inputNomeContato.value = "";
     inputDDDContato.value = "";
     inputTelContato.value = "";
+    inputNumeroContato = "";
 }
 
 function atualizaTabela() {
